97a573bf39ea1d5739f49b0df6ee6c5dfcdd0095
yuudev14 <takaki.yu.dev@gmail.com>
2024-10-17 23:29:38 +0900

    setup sender to msg queue

--- api/workflow/v1/controller.go
+++ api/workflow/v1/controller.go
@@ -1,16 +1,19 @@
 package workflow_controller_v1
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"github.com/streadway/amqp"
 	"github.com/yuudev14-workflow/workflow-service/db"
 	"github.com/yuudev14-workflow/workflow-service/dto"
 	"github.com/yuudev14-workflow/workflow-service/models"
 	"github.com/yuudev14-workflow/workflow-service/pkg/logging"
+	"github.com/yuudev14-workflow/workflow-service/pkg/mq"
 	rest "github.com/yuudev14-workflow/workflow-service/pkg/rests"
 	"github.com/yuudev14-workflow/workflow-service/pkg/types"
 	"github.com/yuudev14-workflow/workflow-service/service"
@@ -334,45 +337,27 @@ func (w *WorkflowController) Trigger(c *gin.Context) {
 		"tasks": tasks,
 		"edges": graph,
 	})
-	// response := rest.Response{C: c}
-	// graph := map[string][]string{
-	// 	"A": {"B"},
-	// 	"B": {"C"},
-	// 	"C": {},
-	// }
 
-	// currentNode := "A"
+	// Publish a message to the queue
 
-	// currentQueue := []string{
-	// 	"A",
-	// }
+	jsonData, jsonErr := json.Marshal(graph)
 
-	// // Publish a message to the queue
-	// body := utils.WorkflowData{
-	// 	Graph:        graph,
-	// 	CurrentNode:  currentNode,
-	// 	CurrentQueue: currentQueue,
-	// 	Visited:      currentQueue,
-	// }
+	if jsonErr != nil {
+		response.ResponseError(http.StatusBadGateway, jsonErr.Error())
+	}
+	err := mq.MQChannel.Publish(
+		"",                  // exchange
+		mq.SenderQueue.Name, // routing key
+		false,               // mandatory
+		false,               // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(jsonData),
+		})
+	if err != nil {
+		response.ResponseError(http.StatusBadGateway, err.Error())
+	}
 
-	// jsonData, jsonErr := json.Marshal(body)
-
-	// if jsonErr != nil {
-	// 	response.ResponseError(http.StatusBadGateway, jsonErr.Error())
-	// }
-	// err := mq.MQChannel.Publish(
-	// 	"",                  // exchange
-	// 	mq.SenderQueue.Name, // routing key
-	// 	false,               // mandatory
-	// 	false,               // immediate
-	// 	amqp.Publishing{
-	// 		DeliveryMode: amqp.Persistent,
-	// 		ContentType:  "text/plain",
-	// 		Body:         []byte(jsonData),
-	// 	})
-	// if err != nil {
-	// 	response.ResponseError(http.StatusBadGateway, err.Error())
-	// }
-
-	// response.ResponseSuccess(gin.H{})
+	response.ResponseSuccess(gin.H{})
 }
