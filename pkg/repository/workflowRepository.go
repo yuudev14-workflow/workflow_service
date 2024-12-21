ba09ea503c8f9f8b2621284aed264c841cd5d1c5
yuudev14 <takaki.yu.dev@gmail.com>
2024-12-21 23:24:00 +0900

    fix when task and edgeses in nullable

--- pkg/repository/workflowRepository.go
+++ pkg/repository/workflowRepository.go
@@ -16,14 +16,14 @@ import (
 )
 
 type WorkflowsGraph struct {
-	ID          uuid.UUID       `db:"id" json:"id"`
-	Name        string          `db:"name" json:"name"`
-	Description *string         `db:"description" json:"description"`
-	TriggerType string          `json:"trigger_type" db:"trigger_type"`
-	CreatedAt   time.Time       `db:"created_at" json:"created_at"`
-	UpdatedAt   time.Time       `db:"updated_at" json:"updated_at"`
-	Tasks       json.RawMessage `db:"tasks" json:"tasks"`
-	Edges       json.RawMessage `db:"edges" json:"edges"`
+	ID          uuid.UUID        `db:"id" json:"id"`
+	Name        string           `db:"name" json:"name"`
+	Description *string          `db:"description" json:"description"`
+	TriggerType *string          `json:"trigger_type" db:"trigger_type"`
+	CreatedAt   time.Time        `db:"created_at" json:"created_at"`
+	UpdatedAt   time.Time        `db:"updated_at" json:"updated_at"`
+	Tasks       *json.RawMessage `db:"tasks" json:"tasks"`
+	Edges       *json.RawMessage `db:"edges" json:"edges"`
 }
 
 type WorkflowRepository interface {
